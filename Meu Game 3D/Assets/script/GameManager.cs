a7b33aa2c9f457157f869800d7d513921f59ae21
Kaio-2112 <k.manoel@escolar.ifrn.edu.br>
2024-09-09 17:03:59 -0300

    Interface, TextMeshPro e GameManager

---
+++ Meu Game 3D/Assets/script/GameManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    public TextMeshProUGUI hud, msgVitoria;
+    public int restantes;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
